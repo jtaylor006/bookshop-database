22f4572fe2d54abad5b1eedd7f2212cbfdaa9dac
jtaylor6 <jarrede20@gmail.com>
2021-01-26 20:44:40 -0500

    created backend database

--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import backend
 window = Tk()
 
 l1 = Label(window, text="Title")
@@ -34,27 +34,27 @@ list1 = Listbox(window, height=6, width=35)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1 = Scrollbar(window)
-sb1.grid(row=2,column=2,rowspan=6)
+sb1.grid(row=2, column=2, rowspan=6)
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window,text="View All", width=12)
-b1.grid(row=2,column=3)
+b1 = Button(window, text="View All", width=12)
+b1.grid(row=2, column=3)
 
-b1=Button(window,text="Search Entry", width=12)
-b1.grid(row=3,column=3)
+b1 = Button(window, text="Search Entry", width=12)
+b1.grid(row=3, column=3)
 
-b1=Button(window,text="Add Entry", width=12)
-b1.grid(row=4,column=3)
+b1 = Button(window, text="Add Entry", width=12)
+b1.grid(row=4, column=3)
 
-b1=Button(window,text="Update Selected", width=12)
-b1.grid(row=5,column=3)
+b1 = Button(window, text="Update Selected", width=12)
+b1.grid(row=5, column=3)
 
-b1=Button(window,text="Delete Selected", width=12)
-b1.grid(row=6,column=3)
+b1 = Button(window, text="Delete Selected", width=12)
+b1.grid(row=6, column=3)
 
-b1=Button(window,text="Close", width=12)
-b1.grid(row=7,column=3)
+b1 = Button(window, text="Close", width=12)
+b1.grid(row=7, column=3)
 
 window.mainloop()
